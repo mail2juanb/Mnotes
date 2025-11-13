df720349a00b6f167cea19516c6eff9a8becfc5d
juanb <mail2juanb@yahoo.fr>
2025-11-13 17:59:51 +0100

    Initial Commit

---
+++ src/main/java/com/microdiab/mnotes/MnotesApplication.java
@@ -0,0 +1,16 @@
+package com.microdiab.mnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableDiscoveryClient
+public class MnotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MnotesApplication.class, args);
+    }
+
+}

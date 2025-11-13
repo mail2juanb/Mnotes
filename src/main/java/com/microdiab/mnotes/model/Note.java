374ac19d1d98ebcb26722f9b4008473ad69a2748
juanb <mail2juanb@yahoo.fr>
2025-11-13 18:01:10 +0100

    Add Note Model

---
+++ src/main/java/com/microdiab/mnotes/model/Note.java
@@ -0,0 +1,79 @@
+package com.microdiab.mnotes.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+
+@Document(collection = "collection_notes")
+public class Note {
+
+    @Id
+    private String id;      // Identifiant unique généré par MongoDB (ObjectId mappé en String)
+
+    @NotNull(message = "patId cannot be null")
+    @Positive(message = "patId must be a positive number")
+    private Long patId;     // Clé de correspondance avec ta base SQL
+
+    @NotBlank(message = "patient name is mandatory")
+    private String patient; // Nom du patient (optionnel, selon tes besoins)
+
+    private String note;    // Champ texte pour la note (supporte les retours à la ligne)
+
+
+
+    public Note() {
+    }
+
+    public Note(String id, Long patId, String patient, String note) {
+        this.id = id;
+        this.patId = patId;
+        this.patient = patient;
+        this.note = note;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public @NotNull(message = "patId cannot be null") @Positive(message = "patId must be a positive number") Long getPatId() {
+        return patId;
+    }
+
+    public void setPatId(@NotNull(message = "patId cannot be null") @Positive(message = "patId must be a positive number") Long patId) {
+        this.patId = patId;
+    }
+
+    public @NotBlank(message = "patient name is mandatory") String getPatient() {
+        return patient;
+    }
+
+    public void setPatient(@NotBlank(message = "patient name is mandatory") String patient) {
+        this.patient = patient;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "id='" + id + '\'' +
+                ", patId=" + patId +
+                ", patient='" + patient + '\'' +
+                ", note='" + note + '\'' +
+                '}';
+    }
+}
